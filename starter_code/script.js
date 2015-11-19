b7dca5a2dd5a6cf547a8a3c3a09f1b1822ae583e
ousman245 <ocamara@cristoreyny.org>
2015-11-19 13:16:18 +0000

    Finishd the project, added CSS and updated my project to show a gif.

--- starter_code/script.js
+++ starter_code/script.js
@@ -3,18 +3,33 @@ var aiPoint = 0;
 
 // This function returns the selection of the computer
 function getAISelection() {
-    //TODO: randomly choose between 'rock', 'paper', or 'scissors'
-}
+    
+var cars = ["rock", "paper", "scissors"];
+var ousman = Math.floor(Math.random()*3);
+return cars[ousman];
+
+};
 
 // This function picks the winner
 function pickWinner(userValue, aiValue) {
-    //TODO: pick the correct winner: user or ai
+    if( userValue == aiValue ){
+        return "draw";
+    } else if (aiValue == "paper" && userValue == "rock" ){
+        return "ai";
+    } else if (aiValue == "rock" && userValue == "scissors"){
+        return "ai";
+    } else if(aiValue == "scissors" && userValue == "paper"){
+        return "ai";
+    } else{
+        return "user";
+    }
+    
+};
     //TODO: Add one point for the winner
-}
-
 // This function sets the scoreboard with the correct points
 function setScore() {
-
+    $("#userPoint").text(userPoint);
+    $("#aiPoint").text(aiPoint);
 }
 
 // This function captures the click and picks the winner
@@ -25,15 +40,21 @@ function evaluate(evt) {
     var winner = pickWinner(userValue, aiValue);
 
     if ( 'user' === winner ) {
-        $('#message').delay(50).text('You have won!, Click a box to play again');
+        $('#message').delay(50).text('You have won!, Click a box to play again'); 
+        userPoint++;
     } else if ( winner === 'draw' ) {
         $('#message').delay(50).text('Draw! Click a box to play again');
     } else {
         $('#message').delay(50).text('You have lost!, Click a box to play again');
+        aiPoint++;
     }
+    setScore();
 }
 
 // This function runs on page load
 $(document).ready(function(){
-
+   $(".token").click(function(evt){
+       evaluate(evt);
+   });
+    
 });
